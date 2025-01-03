9e8f2f689cb1b76d7e0a55f618f9805719cd74ea
Ömer Can Şimşek <108125233+Fri3m@users.noreply.github.com>
2025-01-03 21:47:42 +0300

    Update viewsandprocedures.sql

--- sql files/viewsandprocedures.sql
+++ sql files/viewsandprocedures.sql
@@ -147,7 +147,7 @@ BEGIN
 END
 
 GO
-CREATE PROCEDURE pro_CREATELICENCE
+CREATE PROCEDURE pro_CREATE_LICENCE
 	@LicenceTermPar int,
 	@FeePar money,
 	@StartDatePar datetime,
@@ -174,32 +174,4 @@ BEGIN
 
 END
 
-GO
-CREATE PROCEDURE pro_CREATELICENCE
-  @LicenceTermPar int,
-  @FeePar money,
-  @StartDatePar datetime,
-  @ProductNamePar nvarchar(50),
-  @CompanyNamePar nvarchar(50)
-
-  AS
-  BEGIN
-
-      insert into INVOICE(Fee) values
-  (@FeePar)
-
-      insert into LICENCE(LicenceTerm,StartDate,InvoiceID,ProductID,CompanyID) values
-  (@LicenceTermPar, @StartDatePar,
-  (select max(i.InvoiceID)
-  from INVOICE i
-  ),
-  (select p.productID 
-  from PRODUCT p
-  where p.PName = @ProductNamePar),
-  (select c.CompanyID
-  from COMPANY c 
-  where c.CompanyName = @CompanyNamePar)
-  )
-
-END
 
