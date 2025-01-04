869d1f02f1c60686f6442861ebc7fd333a859b51
Ömer Can Şimşek <108125233+Fri3m@users.noreply.github.com>
2025-01-04 20:24:05 +0300

    Update view feature Request and bug report for manager.sql

--- sql files/View/view feature Request and bug report for manager.sql
+++ sql files/View/view feature Request and bug report for manager.sql
@@ -4,18 +4,19 @@ create procedure pro_VIEW_BUG_REPORTS_MANAGER
 	@managerMail varchar(100)
 AS
 BEGIN
-	SELECT pro.PName, br.VersionID, fb.Message_, cmp.CompanyName, fb.FeedbackDate
-	FROM FEEDBACK fb inner join BUGREPORT br on fb.FeedbackID = br.FeedbackID
-		inner join Company cmp on cmp.CompanyID = fb.CompanyID
-		inner join PRODUCT_ pro on pro.ProductID = fb.ProductID
-	where fb.ProductID in (
-		select p.ProductID
-		from EMPLOYEE emp inner join PERSON per on emp.EmployeeID = per.PersonID
-			inner join EMPLOYEE_PRODUCT ep on ep.EmployeeID = emp.EmployeeID
-			inner join PRODUCT_ p on p.ProductID = ep.ProductID
-		where per.Email = @managerMail
+	print (
+		SELECT pro.PName, br.VersionID, fb.Message_, cmp.CompanyName, fb.FeedbackDate
+		FROM FEEDBACK fb inner join BUGREPORT br on fb.FeedbackID = br.FeedbackID
+			inner join Company cmp on cmp.CompanyID = fb.CompanyID
+			inner join PRODUCT_ pro on pro.ProductID = fb.ProductID
+		where fb.ProductID in (
+			select p.ProductID
+			from EMPLOYEE emp inner join PERSON per on emp.EmployeeID = per.PersonID
+				inner join EMPLOYEE_PRODUCT ep on ep.EmployeeID = emp.EmployeeID
+				inner join PRODUCT_ p on p.ProductID = ep.ProductID
+			where per.Email = @managerMail
 	)
-	order by pro.PName
+	order by pro.PName)
 END
 GO
 
@@ -27,18 +28,20 @@ create procedure pro_VIEW_FEATURE_REQUESTS_MANAGER
 	@managerMail varchar(100)
 AS
 BEGIN
-	SELECT pro.PName, fr.Rating, fb.Message_, cmp.CompanyName, fb.FeedbackDate
-	FROM FEEDBACK fb inner join FEATUREREQUEST fr on fb.FeedbackID = fr.FeedbackID
-		inner join Company cmp on cmp.CompanyID = fb.CompanyID
-		inner join PRODUCT_ pro on pro.ProductID = fb.ProductID
-	where fb.ProductID in (
-		select p.ProductID
-		from EMPLOYEE emp inner join PERSON per on emp.EmployeeID = per.PersonID
-			inner join EMPLOYEE_PRODUCT ep on ep.EmployeeID = emp.EmployeeID
-			inner join PRODUCT_ p on p.ProductID = ep.ProductID
-		where per.Email = @managerMail
+	print (
+		SELECT pro.PName, fr.Rating, fb.Message_, cmp.CompanyName, fb.FeedbackDate
+		FROM FEEDBACK fb inner join FEATUREREQUEST fr on fb.FeedbackID = fr.FeedbackID
+			inner join Company cmp on cmp.CompanyID = fb.CompanyID
+			inner join PRODUCT_ pro on pro.ProductID = fb.ProductID
+		where fb.ProductID in (
+			select p.ProductID
+			from EMPLOYEE emp inner join PERSON per on emp.EmployeeID = per.PersonID
+				inner join EMPLOYEE_PRODUCT ep on ep.EmployeeID = emp.EmployeeID
+				inner join PRODUCT_ p on p.ProductID = ep.ProductID
+			where per.Email = @managerMail
+		)
+		order by pro.PName
 	)
-	order by pro.PName
 END
 GO
 
@@ -49,19 +52,21 @@ create procedure pro_VIEW_AVG_RATING_MANAGER
 	@managerMail varchar(100)
 AS
 BEGIN
-	SELECT pro.PName, avg(fr.Rating)
-	FROM FEEDBACK fb inner join FEATUREREQUEST fr on fb.FeedbackID = fr.FeedbackID
-		inner join Company cmp on cmp.CompanyID = fb.CompanyID
-		inner join PRODUCT_ pro on pro.ProductID = fb.ProductID
-	where fb.ProductID in (
-		select p.ProductID
-		from EMPLOYEE emp inner join PERSON per on emp.EmployeeID = per.PersonID
-			inner join EMPLOYEE_PRODUCT ep on ep.EmployeeID = emp.EmployeeID
-			inner join PRODUCT_ p on p.ProductID = ep.ProductID
-		where per.Email = @managerMail
+	print (
+		SELECT pro.PName, avg(fr.Rating)
+		FROM FEEDBACK fb inner join FEATUREREQUEST fr on fb.FeedbackID = fr.FeedbackID
+			inner join Company cmp on cmp.CompanyID = fb.CompanyID
+			inner join PRODUCT_ pro on pro.ProductID = fb.ProductID
+		where fb.ProductID in (
+			select p.ProductID
+			from EMPLOYEE emp inner join PERSON per on emp.EmployeeID = per.PersonID
+				inner join EMPLOYEE_PRODUCT ep on ep.EmployeeID = emp.EmployeeID
+				inner join PRODUCT_ p on p.ProductID = ep.ProductID
+			where per.Email = @managerMail
+		)
+		group by pro.PName
+		order by pro.PName
 	)
-	group by pro.PName
-	order by pro.PName
 END
 GO
 
@@ -70,17 +75,19 @@ CREATE PROCEDURE pro_VIEW_AVG_RATING_PRODUCT
 	@productName varchar(100)
 AS
 BEGIN
-	SELECT avg(fr.Rating)
-	FROM FEEDBACK fb inner join FEATUREREQUEST fr on fb.FeedbackID = fr.FeedbackID
-		inner join Company cmp on cmp.CompanyID = fb.CompanyID
-		inner join PRODUCT_ pro on pro.ProductID = fb.ProductID
-	where fb.ProductID in (
-		select p.ProductID
-		from PRODUCT_ p 
-		where p.PName = @productName
+	print (
+		SELECT avg(fr.Rating)
+		FROM FEEDBACK fb inner join FEATUREREQUEST fr on fb.FeedbackID = fr.FeedbackID
+			inner join Company cmp on cmp.CompanyID = fb.CompanyID
+			inner join PRODUCT_ pro on pro.ProductID = fb.ProductID
+		where fb.ProductID in (
+			select p.ProductID
+			from PRODUCT_ p 
+			where p.PName = @productName
+		)
+		group by pro.PName
+		order by pro.PName
 	)
-	group by pro.PName
-	order by pro.PName
 END
 GO
 
